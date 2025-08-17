56bb391b26bdd5cc0856dfc365729c91366bda03
isra-bouzidi <b.isra2004@gmail.com>
2025-08-17 22:08:13 +0100

    Backend refactor

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-import todoRoutes from './routes/todo.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import todoRoutes from "./routes/todo.route.js";
+import { connectDB } from "./config/db.js";
+import path from "path";
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -9,10 +11,18 @@ const app = express();
 
 app.use(express.json());
 
-
 app.use("/api/todos", todoRoutes);
 
-app.listen(5000, () => {
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server started at http://localhost:5000');
-})
+  console.log("Server started at http://localhost:5000");
+});
